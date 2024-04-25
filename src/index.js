bc4eb108e9ea7812e7df09446838fbbd02e43dd6
MatteoFranzolin <matteo.franzolin@iisviolamarchesini.edu.it>
2024-04-25 17:31:44 +0200

    aggiunte per post e put

--- src/index.js
+++ src/index.js
@@ -7,14 +7,25 @@ const server_address = "127.0.0.1:8081"
 function handleRequest(method) {
     return function() {
         const id = document.getElementById("id_input").value;
+        let data = null;
         if (method === 'post' || method === 'put') {
             const nome = document.getElementById("nome_input").value;
             const marca = document.getElementById("marca_input").value;
             const prezzo = document.getElementById("prezzo_input").value;
+            data = {
+                type: "products",
+                attributes:{
+                    nome: nome,
+                    marca: marca,
+                    prezzo: prezzo
+                }
+            };
+            console.log(data);
         }
         const url = id ? `http://${server_address}/products/${id}` : `http://${server_address}/products`; //se c'è id allora fa richiesta con id, altrimenti senza
         const root = ReactDOM.createRoot(document.getElementById('root'));
-        root.render(<Mapper url={url} method={method}></Mapper>); //richiama mapper con metodo e url corrispondente
+
+        root.render(<Mapper url={url} method={method} data={data}></Mapper>); //richiama mapper con metodo e url corrispondente
     }
 }
 
